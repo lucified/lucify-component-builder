4b9d45043030fae8b0f0104d5badc6d7e8177ae8
Juho Ojala <juho@lucify.com>
2016-01-11 15:12:58 +0200

    Add simple test project

---
+++ test-projects/embed-test-project/gulpfile.babel.js
@@ -0,0 +1,15 @@
+
+var gulp = require('gulp');
+
+
+var opts = {
+	paths: ['node_modules/lucify-commons', 'test_modules/module1'],
+	publishFromFolder: 'dist',
+	defaultBucket: 'lucify-dev',
+	maxAge: 3600,
+	assetContext: 'embed/taxes-main-content-v2/',
+	baseUrl: 'http://dev.lucify.com/',
+}
+
+var builder = require('../../index.js'); // lucify-component-builder
+builder(gulp, opts);
