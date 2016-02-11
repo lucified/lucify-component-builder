308b1a3d930bce2005527f3da34e425b1a202aaf
Juho Ojala <juho@lucify.com>
2016-02-11 17:44:59 +0000

    Add some basic fileExists checks for test-project builds

--- test/tests.js
+++ test/tests.js
@@ -5,6 +5,8 @@ var fs = require('fs')
 var builder = require('../')
 var ENVS = require('../src/js/envs.js')
 
+var fileExists = require('file-exists');
+
 const deployOpt = require('../src/js/deploy-options.js')
 
 
@@ -40,7 +42,7 @@ describe("github-deploy", done => {
 })
 
 describe("build test projects", () => {
-  it("works", done => {
+  it("returns exit code zero for builds", done => {
     const parent = require('path').normalize(__dirname + "/../")
     const spawn = require('child_process').spawn;
     const p = spawn('./build-test-projects', [], {cwd: parent});
@@ -58,7 +60,39 @@ describe("build test projects", () => {
       if(code > 0) return done(new Error("Failed"))
       done()
     });
-
-
   })
+
+  it("creates basic assets for single-page-test-project", done => {
+    fileExists('test-projects/single-page-test-project/dist/hello-world/index.html').should.equal(true);
+    fileExists('test-projects/single-page-test-project/dist/index.html').should.equal(false);
+    done();
+  });
+
+  it("creates basic assets for single-embed-test-project", done => {
+    fileExists('test-projects/single-embed-test-project/dist/embed/hello-world/index.html').should.equal(true);
+    fileExists('test-projects/single-embed-test-project/dist/embed/hello-world/embed-codes.html').should.equal(true);
+    fileExists('test-projects/single-embed-test-project/dist/embed/hello-world/embed.js').should.equal(true);
+    fileExists('test-projects/single-embed-test-project/dist/embed/hello-world/resize.js').should.equal(true);
+    fileExists('test-projects/single-embed-test-project/dist/index.html').should.equal(false);
+    done();
+  });
+
+  it("creates basic assets for multi-embed-test-project", done => {
+    fileExists('test-projects/multi-embed-test-project/dist/embed/hello-world/index.html').should.equal(true);
+    fileExists('test-projects/multi-embed-test-project/dist/embed/hello-world/embed-codes.html').should.equal(true);
+    fileExists('test-projects/multi-embed-test-project/dist/embed/subpath/hello-world-two/index.html').should.equal(true);
+    fileExists('test-projects/multi-embed-test-project/dist/embed/subpath/hello-world-two/embed-codes.html').should.equal(true);
+    fileExists('test-projects/multi-embed-test-project/dist/embed/embed.js').should.equal(true);
+    fileExists('test-projects/multi-embed-test-project/dist/embed/resize.js').should.equal(true);
+    fileExists('test-projects/multi-embed-test-project/dist/embed/index.html').should.equal(false);
+    done();
+  });
+
+  it("creates basic assets for multi-page-test-project", done => {
+    fileExists('test-projects/multi-page-test-project/dist/test-path/index.html').should.equal(true);
+    fileExists('test-projects/multi-page-test-project/dist/test-path/embed-codes.html').should.equal(false);
+    fileExists('test-projects/multi-page-test-project/dist/test-path/subpage/index.html').should.equal(true);
+    done();
+  });
+
 })
