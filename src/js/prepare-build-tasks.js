eb552f208578627b80cfe5df89516f10e3784bca
Juho Ojala <juho@lucify.com>
2016-04-18 16:42:38 +0100

    Add option for providing the js bundle entry point directly

--- src/js/prepare-build-tasks.js
+++ src/js/prepare-build-tasks.js
@@ -101,17 +101,22 @@ function generateMetaData(path) {
  * Create JSX and run webpack to create the associated bundle
  *
  * destPath      -- path below which all files are created
- * componentPath -- path to React component to be bundled
+ * entryPoint    -- path to entry point for bundle
+ * componentPath -- path to React component to be bundled (only applies if entry point not defined)
  * reactRouter   -- enable react-router
  * pageDefs      -- page definitions for creating associated html files
  * watch         -- start watching with webback-dev-server
  *
  */
-function createJsxAndBundle(destPath, componentPath, reactRouter, pageDefs, watch, assetContext, babelPaths, callback) {
-  var tempFileName = getTempFileName(componentPath);
-  generateJSX(reactRouter, componentPath, tempFileName);
+function createJsxAndBundle(destPath, entryPoint, componentPath, reactRouter, pageDefs, watch, assetContext, babelPaths, callback) {
+
+  if (!entryPoint) {
+    var tempFileName = getTempFileName(componentPath);
+    generateJSX(reactRouter, componentPath, tempFileName);
+    entryPoint = './temp/' + tempFileName;
+  }
+
   generateMetaData(destPath);
-  var entryPoint = './temp/' + tempFileName;
   bundleWebpack(
     entryPoint,
     null,
@@ -160,7 +165,7 @@ function bundleComponents(opts, context, assetContext, callback) {
   if (!opts.embedDefs) {
     var watch = context.dev; // TODO
     var componentPath = 'index.js';
-    createJsxAndBundle(context.destPath, componentPath,
+    createJsxAndBundle(context.destPath, opts.entryPoint, componentPath,
       opts.reactRouter, pageDefs, watch, assetContext, opts.babelPaths, callback);
     return;
   }
@@ -176,7 +181,7 @@ function bundleComponents(opts, context, assetContext, callback) {
     var watch = false;
     var pageDef = pageDefUtils.enrichPageDef(edef, opts.baseUrl, assetContext);
     pageDef.path = '';
-    return createJsxAndBundlePromisified(destPath, edef.componentPath,
+    return createJsxAndBundlePromisified(destPath, null, edef.componentPath,
       opts.reactRouter, [pageDef], watch, assetContext + edef.path.substring(1) + '/', opts.babelPaths);
   });
 
