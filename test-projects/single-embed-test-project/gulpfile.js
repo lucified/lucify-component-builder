2000b54efba5c4d682a1ebf2223a29478850497a
Juho Ojala <juho@lucify.com>
2016-05-18 11:11:09 +0300

    Add embedSupport option to test projects

--- test-projects/single-embed-test-project/gulpfile.js
+++ test-projects/single-embed-test-project/gulpfile.js
@@ -5,6 +5,7 @@ var path = require('path');
 var opts = {
   assetContext: 'embed/hello-world/',
   entryPoint: path.resolve('src/js/entry-point.jsx'),
+  embedSupport: true
 }
 
 var builder = require('../../index.js'); // lucify-component-builder
